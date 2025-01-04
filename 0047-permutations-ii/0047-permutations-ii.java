2424c54e6c08ffda40208835b2a061a5d80a8397
AchalVishnoi <161050000+AchalVishnoi@users.noreply.github.com>
2025-01-04 15:26:10 +0530

    Time: 1 ms (99.85%), Space: 44.9 MB (45.17%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,44 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+
+        Arrays.sort(nums);
+
+        List<List<Integer>> ans=new ArrayList<>();
+
+         permutation(nums,ans,new ArrayList<>());
+         return ans;
+        
+    }
+
+     public void permutation(int a[],List<List<Integer>> ans,List<Integer> com){
+
+      
+
+       
+
+        if(com.size()==a.length){
+            ans.add(new ArrayList<>(com));
+            return;
+
+        }
+
+
+        for(int i=0;i<a.length;i++){
+
+            if(a[i]==11||(i>0&&a[i]==a[i-1])) continue;
+            com.add(a[i]);
+            int temp=a[i];
+            a[i]=11;
+            permutation(a,ans,com);
+            a[i]=temp;
+            com.remove(com.size()-1);
+
+
+        }
+
+
+      
+
+
+    }
+}
