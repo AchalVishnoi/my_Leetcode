d4280fa86cc64342dbbbc9d3d253fd841c449b84
AchalVishnoi <161050000+AchalVishnoi@users.noreply.github.com>
2025-02-17 22:56:51 +0530

    Time: 2 ms (76.63%), Space: 44.4 MB (66.91%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,28 +1,67 @@
-import java.util.HashSet;
-import java.util.Set;
-
 class Solution {
     public boolean isValidSudoku(char[][] b) {
-        Set<String> s = new HashSet<>();
+
+
+
+
+        // to check row and col
+
         
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (b[i][j] != '.') {
-                    String num = String.valueOf(b[i][j]);
+        for(int i=0;i<9;i++){
+            Set <Character> s=new HashSet<>();
 
-                    if (s.contains(num + " in row " + i) ||
-                        s.contains(num + " in col " + j) ||
-                        s.contains(num + " in box " + (i / 3) + "-" + (j / 3))) {
-                        return false;
-                    }
-
-                    s.add(num + " in row " + i);
-                    s.add(num + " in col " + j);
-                    s.add(num + " in box " + (i / 3) + "-" + (j / 3));
-                }
+            for( int j=0;j<9;j++){
+               
+               if(b[i][j]!='.'){
+                if(s.contains(b[i][j])) return false;
+                s.add(b[i][j]);
+               }
             }
+
         }
 
-        return true;
+
+        
+        for(int i=0;i<9;i++){
+            Set <Character> s=new HashSet<>();
+
+            for( int j=0;j<9;j++){
+               
+               if(b[j][i]!='.'){
+                if(s.contains(b[j][i])) return false;
+                s.add(b[j][i]);
+               }
+            }
+
+        }
+
+
+
+
+       for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+         
+         int strow=i*3,endrow=(i+1)*3-1,stcol=j*3, endcol=(j+1)*3-1;
+           Set <Character> s=new HashSet<>();
+         for(int r=strow;r<=endrow;r++){
+            for(int c=stcol;c<=endcol;c++){
+              if(b[r][c]!='.'){
+                if(s.contains(b[r][c])) return false;
+                s.add(b[r][c]);
+               }
+            }
+         }
+
+
+
+
+        }
+       }
+
+
+
+
+   return true;
+        
     }
 }
