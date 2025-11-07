2985490b7acc710c6246c62b4f6ed3550009e7c2
Achal Vishnoi <161050000+AchalVishnoi@users.noreply.github.com>
2025-11-08 03:41:54 +0530

    Time: 10 ms (14.6%), Space: 47.1 MB (7.94%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -2,12 +2,14 @@ class Solution {
     public boolean canFinish(int n, int[][] pre) {
 
         Map<Integer,List<Integer>> adj=new HashMap<>();
+        int cnt[]=new int[n];
         for(int []p:pre){
             List <Integer> l=adj.getOrDefault(p[1],new ArrayList<>());
             l.add(p[0]);
             adj.put(p[1],l);
+            cnt[p[0]]++;
         }
-        boolean vis[]=new boolean[n];
+     /* boolean vis[]=new boolean[n];
         boolean recvis[]=new boolean[n];
         
         for(int i=0;i<n;i++){
@@ -16,8 +18,36 @@ class Solution {
             
         }
 
-        return true;
+        */
 
+        boolean isFind[]=new boolean[n];
+        Queue<Integer> q=new LinkedList<>();
+        int num=0;
+        for(int i=0;i<n;i++){
+            if(cnt[i]==0){
+                q.add(i);
+            }
+        }
+
+        while(!q.isEmpty()){
+            int a=q.poll();
+            if(!isFind[a]){
+                num++;
+                isFind[a]=true;
+            }
+            else return false;
+
+           if(adj.get(a)!=null){
+            for(int i:adj.get(a)){
+                cnt[i]--;
+                if(cnt[i]==0) q.add(i);
+            }
+           }
+
+        }
+
+        if(num!=n) return false;
+        return true;
         
     }
 
