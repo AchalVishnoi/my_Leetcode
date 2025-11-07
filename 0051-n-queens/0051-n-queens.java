c1bad3d01c46416f1c17bf061b0c634e23943d56
Achal Vishnoi <161050000+AchalVishnoi@users.noreply.github.com>
2025-11-08 05:28:17 +0530

    Time: 3 ms (41.93%), Space: 46.9 MB (6%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-      List<List<String>> result = new ArrayList<>();
+        List<List<String>> result = new ArrayList<>();
         char[][] board = new char[n][n];
 
         for (char[] row : board) {
@@ -12,37 +12,25 @@ class Solution {
 
     }
 
+    public void solve(char board[][], int row, List<List<String>> ans) {
 
-    public void solve (char board[][],int row,List<List<String>> ans){
-       
-     if(row>=board.length){
-       
-       ans.add(constructBoard(board));
-       return;
-     }
-     
+        if (row >= board.length) {
 
+            ans.add(constructBoard(board));
+            return;
+        }
 
-     for(int col=0;col<board[0].length;col++){
-       
-       
-       if(isValid(board,row,col)){
-        board[row][col]='Q';
-        solve(board,row+1,ans);
-        board[row][col]='.';
-       }
-       
+        for (int col = 0; col < board[0].length; col++) {
 
-     }
+            if (isValid(board, row, col)) {
+                board[row][col] = 'Q';
+                solve(board, row + 1, ans);
+                board[row][col] = '.';
+            }
 
-
-
-
+        }
 
     }
-
-
-  
 
     private List<String> constructBoard(char[][] board) {
         List<String> res = new ArrayList<>();
@@ -52,27 +40,27 @@ class Solution {
         return res;
     }
 
+    private boolean isValid(char board[][], int row, int col) {
+        // for left diagonel
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
 
-    private Boolean isValid(char board[][],int row,int col){
-      //for left diagonel
-      for(int i=row,j=col;i>=0&&j>=0;i--,j--){
-       if(board[i][j]=='Q') return false;
-      }
+        // for right diagonal
+        for (int i = row, j = col; i >= 0 && j < board[0].length; i--, j++) {
+            if (board[i][j] == 'Q')
+                return false;
 
-      //for right diagonal
-      for(int i=row,j=col;i>=0&&j<board[0].length;i--,j++){
-      if(board[i][j]=='Q') return false;
+        }
 
-      }
+        // upper side
+        for (int i = row; i >= 0; i--) {
+            if (board[i][col] == 'Q'||board[row][i]=='Q')
+                return false;
 
-     //upper side 
-     for(int i=row;i>=0;i--){
-      if(board[i][col]=='Q') return false;
+        }
 
-      }
-
-
-
-     return true;
+        return true;
     }
 }
