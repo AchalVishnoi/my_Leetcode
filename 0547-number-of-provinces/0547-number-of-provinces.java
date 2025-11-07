18a10b0550cb86c06c4ace8b4d7f9d6755c5ca36
Achal Vishnoi <161050000+AchalVishnoi@users.noreply.github.com>
2025-11-08 04:38:07 +0530

    Time: 5 ms (11.13%), Space: 49.7 MB (5.08%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -2,7 +2,7 @@ class Solution {
     public int findCircleNum(int[][] isC) {
 
         int n=isC.length;
-        int cnt=0;
+ /*       int cnt=0;
 
         Map<Integer,List<Integer> > adj=new HashMap<>();
 
@@ -26,9 +26,11 @@ class Solution {
             }
         }
 
+        */
 
 
-     return cnt;
+
+     return bfs(isC,n);
         
     }
 
@@ -41,5 +43,34 @@ class Solution {
         for(int j:adj.get(i)){
             dfs(j,vis,adj);
         }
+    }
+
+    public int bfs(int [][] isC,int n){
+        Queue<Integer> q=new LinkedList<>();
+        int cnt=0;
+        boolean vis[]=new boolean[n];
+        for(int i=0;i<n;i++){
+          
+          if(vis[i]) continue;
+          cnt++;
+          q.add(i);
+          while(!q.isEmpty()){
+            int city=q.poll();
+            vis[city] =true;
+            for(int j=0;j<n;j++){
+                if(vis[j]) continue;
+                if(isC[city][j]==1)
+                q.add(j);
+            }
+
+
+          }
+
+          
+            
+
+        }
+
+        return cnt;
     }
 }
