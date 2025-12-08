57897315f962a8da90f6cb132a78b75d1d34bdf1
Achal Vishnoi <161050000+AchalVishnoi@users.noreply.github.com>
2025-12-09 02:18:01 +0530

    Time: 5 ms (60.89%), Space: 47.1 MB (39.01%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,30 +1,31 @@
 class Solution {
-    int ans=Integer.MAX_VALUE;
-    Integer dp[][];
+    
     public int minDistance(String w1, String w2) {
-      int n=w1.length();
-      int m=w2.length();       
-        dp=new Integer[n][m];
-        return find(w1,w2,0,0);
+        int n=w1.length();
+        int m=w2.length();       
+        int[][]dp=new int[n+1][m+1];
+
+        for(int i=0;i<n;i++){
+            dp[i][m]=n-i;
+        }
+        for(int i=0;i<m;i++){
+            dp[n][i]=m-i;
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(w1.charAt(i)==w2.charAt(j)) dp[i][j]=dp[i+1][j+1];
+                else{
+                    dp[i][j]=dp[i+1][j+1];
+                    dp[i][j]=Math.min(dp[i+1][j],dp[i][j]);
+                    dp[i][j]=Math.min(dp[i][j+1],dp[i][j]);
+                    dp[i][j]++;
+                }
+            }
+        }
+
+        return dp[0][0];
+
         
     }
-    public int find(String w1,String w2,int i,int j){
 
-      int n=w1.length();
-      int m=w2.length();
-      if(i>=n) return m-j;
-      if(j>=m) return n-i;
-
-      if(dp[i][j]!=null) return dp[i][j];
-
-      if(w1.charAt(i)==w2.charAt(j)) return dp[i][j]=find(w1,w2,i+1,j+1);
-
-      int a=find(w1,w2,i+1,j+1);
-        a=Math.min(find(w1,w2,i+1,j),a);
-        a=Math.min(find(w1,w2,i,j+1),a);
-      
-
-      return dp[i][j]=a+1;
-
-    }
 }
