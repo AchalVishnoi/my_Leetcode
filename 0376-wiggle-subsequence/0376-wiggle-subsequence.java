f0281dfbca4e03d785a001bb8f5470694c4b985c
Achal Vishnoi <161050000+AchalVishnoi@users.noreply.github.com>
2025-12-14 23:34:02 +0530

    Time: 0 ms (100%), Space: 42.7 MB (67.85%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.java
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.java
@@ -3,33 +3,14 @@ class Solution {
 
 
         int n=nums.length;
-        int pos[]=new int[n];
-        int neg[]=new int[n];
 
-        pos[0]=1;
-        neg[0]=1;
-
-        int ans=1;
-
+        int up=1,down=1;
         for(int i=1;i<n;i++){
-            pos[i]=1;
-            neg[i]=1;
-            for(int j=i-1;j>=0;j--){
-               if(nums[i]==nums[j]) continue;
-               
-               if(nums[i]>nums[j]){
-                pos[i]=Math.max(pos[i],neg[j]+1);
-               }
-               else{
-                neg[i]=Math.max(neg[i],pos[j]+1);
-               }
-            }
-
-            ans=Math.max(pos[i],ans);
-            ans=Math.max(neg[i],ans);
+            if(nums[i]>nums[i-1]) up=Math.max(down+1,up);
+            if(nums[i]<nums[i-1]) down=Math.max(up+1,down);
         }
 
-        return ans;
+        return Math.max(up,down);
 
 
         
