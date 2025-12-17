08c6040972de24509aa17d20d8faba8b3361b4e8
Achal Vishnoi <161050000+AchalVishnoi@users.noreply.github.com>
2025-12-17 18:09:51 +0530

    Time: 190 ms (32.27%), Space: 285.5 MB (5.32%) - LeetHub

--- 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.java
+++ 3573-best-time-to-buy-and-sell-stock-v/3573-best-time-to-buy-and-sell-stock-v.java
@@ -3,9 +3,46 @@ class Solution {
     public long maximumProfit(int[] p, int k) {
 
         int n=p.length;
-        t=new Long[n][k+1][3];
+        t=new Long[n+1][k+1][3];
 
-        return find(p,0,k,0);
+        for(int i=0;i<=n;i++){
+            t[i][0][0]=0L;
+            t[i][0][1]=(long)Integer.MIN_VALUE;
+            t[i][0][2]=(long)Integer.MIN_VALUE;
+        }
+
+        for(int i=0;i<=k;i++){
+            t[n][i][0]=0L;
+            t[n][i][1]=(long)Integer.MIN_VALUE;
+            t[n][i][2]=(long)Integer.MIN_VALUE;
+        }
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=1;j<=k;j++){
+                for(int c=0;c<3;c++){
+                    long take;
+                    if(c==2){
+                        take=-p[i]+t[i+1][j-1][0];
+                    }
+                    else if(c==1){
+                        take=p[i]+t[i+1][j-1][0];
+                    }
+                    else{
+                        take=Math.max(-p[i]+t[i+1][j][1],p[i]+t[i+1][j][2]);
+                    }
+
+                    long notTake=t[i+1][j][c];
+
+                    t[i][j][c]=Math.max(take,notTake);
+                }
+            }
+        }
+
+
+        return t[0][k][0];
+
+
+
 
 
     }
