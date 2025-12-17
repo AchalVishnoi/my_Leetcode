bd094b32ad4516e4a603c65856e2363d52470354
Achal Vishnoi <161050000+AchalVishnoi@users.noreply.github.com>
2025-12-17 23:50:34 +0530

    Time: 8 ms (22.05%), Space: 43.5 MB (74.53%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -6,22 +6,16 @@ class Solution {
         int dp[]=new int[n+1];
 
         for(int i=n-1;i>=0;i--){
+
+            int max=0;
             for(int j=i;j<Math.min(n,i+k);j++){
-               dp[i]=Math.max(dp[i],max(arr,i,j)*(j-i+1)+dp[j+1]);
+                max=Math.max(max,arr[j]);
+               dp[i]=Math.max(dp[i],max*(j-i+1)+dp[j+1]);
             }
         }
 
         return dp[0];
 
         
-    }
-
-    private int max(int a[],int i,int j){
-        int m=a[i];
-        for(int k=i;k<=j;k++){
-          m=Math.max(a[k],m);
-        }
-
-        return m;
     }
 }
