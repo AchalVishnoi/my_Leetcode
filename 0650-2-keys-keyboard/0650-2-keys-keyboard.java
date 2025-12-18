7f8c957febdaa019a2a36dfdcd6fe904d23617df
Achal Vishnoi <161050000+AchalVishnoi@users.noreply.github.com>
2025-12-18 16:26:20 +0530

    Time: 4 ms (63.36%), Space: 42.7 MB (28.23%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.java
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -3,11 +3,23 @@ class Solution {
 
         if(n==1) return 0;
 
-        Long dp[][]=new Long[n+1][n+1];
+        //int dp[][]=new int[n+1][n+1];
 
-        return (int)find(1,1,n,dp)+1;
+        int dp[]=new int[n+1];
+        dp[0]=0;
+        dp[1]=0;
 
+        for(int i=2;i<=n;i++){
+           dp[i]=i;
+           for(int j=2;j*j<=i;j++){
+              if(i%j==0){
+                dp[i]=Math.min(dp[i],dp[j]+i/j);
+                dp[i]=Math.min(dp[i],dp[i/j]+j);
+              }
+           }
+        }
 
+        return dp[n];
 
         
     }
