e942771167ddbc67b5c1d93e4c8e2a92b3cea587
Achal Vishnoi <161050000+AchalVishnoi@users.noreply.github.com>
2025-12-21 03:04:59 +0530

    Time: 26 ms (64.95%), Space: 46.3 MB (52.73%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.java
+++ 0935-knight-dialer/0935-knight-dialer.java
@@ -3,17 +3,48 @@ class Solution {
     int mod=1000000007;
     public int knightDialer(int n) {
 
-        long ans=0;
-        dp=new Long[4][3][n+1];
-        for(int i=0;i<4;i++){
-            for(int j=0;j<3;j++){
-                ans=(ans+find(i,j,n-1)%mod)%mod;
-            }
-        }
+    int[][] moves = {
+             {4, 6},
+             {6, 8},
+             {7, 9},
+             {4, 8},
+             {0, 3, 9},
+             {},
+             {0, 1, 7},
+             {2, 6},
+             {1, 3},
+             {2, 4}
+                     };
 
-        return (int)ans;
+
+
+
+        long dp[]=new long[10];
+        Arrays.fill(dp,1);
+
+
+        for(int i=2;i<=n;i++){
+            long next[]=new long[10];
+            for(int d=0;d<10;d++){
+                for(int t:moves[d]){
+                    next[t]=(next[t]+dp[d])%mod;
+                }
+            }
+
+            dp=next;
+        } 
+
+
+        long ans=0;
+        for(int i=0;i<10;i++) ans=(ans+dp[i])%mod;
+
+
+        return (int)ans;        
+
+
         
     }
+    
     public long find(int i,int j,int n){
         
         if(i<0||j<0||(i==3&&(j==0||j==2))||j>2||i>3) return 0;
