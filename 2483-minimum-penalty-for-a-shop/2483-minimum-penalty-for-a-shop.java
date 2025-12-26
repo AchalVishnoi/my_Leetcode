bf7b30b10709f42d6cf001f91d8f0bf42727d847
Achal Vishnoi <161050000+AchalVishnoi@users.noreply.github.com>
2025-12-26 11:07:37 +0530

    Time: 10 ms (80.69%), Space: 47.1 MB (32.93%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.java
@@ -2,33 +2,31 @@ class Solution {
     public int bestClosingTime(String s) {
 
         int n=s.length();
-
-        int a[]=new int[n];
-        int b[]=new int[n];
-        a[0]=s.charAt(0)=='N'?1:0;
-        b[n-1]=s.charAt(n-1)=='Y'?1:0;
-        for(int i=1;i<n;i++){
-            a[i]=a[i-1];
-            b[n-i-1]=b[n-i];
-            if(s.charAt(i)=='N') a[i]++;
-            if(s.charAt(n-i-1)=='Y') b[n-i-1]++;
-        }
-
-        int min=b[0];
-
         int j=0;
-        
-        for(int i=1;i<n;i++){
-          if(min>a[i-1]+b[i]){
-            min=a[i-1]+b[i];
-            j=i;
-          }
+        int min=0;
+        int pan=0;
+
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)=='Y') pan++;
         }
-        if(min>a[n-1]){
-          j=n;
+
+        min=pan;
+        j=0;
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)=='Y')pan--;
+            else pan++;
+
+            
+            if(pan<min){
+              j=i+1;
+              min=pan;
+            }
         }
 
         return j;
+
+
+
 
         
     }
