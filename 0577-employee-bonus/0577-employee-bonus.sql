b8c48ac231ae5d898e830d01e70051334eaad4ee
Achal Vishnoi <161050000+AchalVishnoi@users.noreply.github.com>
2025-12-29 19:52:25 +0530

    Time: 794 ms (97.94%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,9 +1,9 @@
-# Write your MySQL query statement below
-
-SELECT E.name, B.bonus
-FROM
-(Employee E LEFT JOIN Bonus B ON E.empId=B.empId)
-
-WHERE E.empId
-NOT IN 
-    (SELECT empId FROM Bonus WHERE bonus>=1000)
+#Write your MySQL query statement below
+select e.name,b.bonus
+from 
+Employee e
+left join
+Bonus b
+on e.empId=b.empId
+where
+b.bonus<1000 or b.bonus is null
