a324a99dc6acf82ba048af7633b279fb03d49da5
Achal Vishnoi <161050000+AchalVishnoi@users.noreply.github.com>
2026-01-04 17:21:18 +0530

    Time: 8 ms (98.74%), Space: 51.1 MB (5.3%) - LeetHub

---
+++ 1390-four-divisors/1390-four-divisors.java
@@ -0,0 +1,39 @@
+class Solution {
+    static Integer[] div=new Integer[1000000];
+    public int sumFourDivisors(int[] nums) {
+
+        int sum=0;
+        for(int x:nums){
+            sum+=find(x);
+        }
+
+        return sum;
+        
+    }
+    public int find(int a){
+        if(div[a]!=null) return div[a];
+
+        int sum=0;
+        int cnt=0;
+        for(int i=1;i*i<=a;i++){
+          if(a%i==0){
+            sum+=i;
+            cnt++;
+            int j=a/i;
+            if (i!=j) {
+                cnt++;
+                sum+=j;
+            }
+          }
+          if(cnt>4){
+            div[a]=0;
+            return 0;
+          }
+        }
+
+        if(cnt<4) div[a]=0;
+        else div[a]=sum;
+
+        return div[a];
+    }
+}
