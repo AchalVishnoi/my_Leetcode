e012aa86f3ee4e037c1d40048918f65d385d4835
Achal Vishnoi <161050000+AchalVishnoi@users.noreply.github.com>
2026-02-05 15:01:59 +0530

    Time: 3 ms (94.02%), Space: 46.8 MB (39.77%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,59 +1,37 @@
 class Solution {
-    public String convert(String s, int numRows) {
-
+    public String convert(String s, int nr) {
         int n=s.length();
+        if(n==1||nr==1) return s;
 
-        if(s.length()<=numRows||numRows==1) return s;
-        StringBuilder str=new StringBuilder();
-        int upperJump,lowerJump;
-        
 
-        for(int i=1;i<=numRows;i++){
-             lowerJump=(2*(numRows-i)-1+1);     //-1 coz last corner element added 2 times
-             upperJump=(i-1)*2-1+1;
+        int upper=2*nr-2;
+        int lower=0;
 
-             int j=i-1;   //index when in numberd row
-             int lowerJumpChance=1;
-             int upperJumpChance=0;
-             while(j<n){
-                
-                if(lowerJump==0) {
-                    lowerJumpChance=0;
-                     upperJumpChance=1;}
 
-                if(upperJump==0){
-                      lowerJumpChance=1;
-                     upperJumpChance=0;
+        StringBuilder st=new StringBuilder();
+        for(int i=0;i<nr;i++){
+            boolean fl=true;
+            int up=upper-2*i;
+            int down=2*i;
+
+            int j=i;
+            while(j<n){
+                st.append(s.charAt(j));
+                if(fl){
+                    if(up==0)j+=down;
+                    else j+=up;
+                } else {
+                    if (down==0) j+=up;
+                    else j+=down;
                 }
-
-
-
-                str.append(s.charAt(j));
-                int jump;
-                if(lowerJumpChance==1){
-                   if(lowerJump!=0) j+=lowerJump;
-                    lowerJumpChance=0;
-                     upperJumpChance=1;
-
-                }
-                else{
-                     if(upperJump!=0)j+=upperJump;
-                    lowerJumpChance=1;
-                     upperJumpChance=0;
-                }
-
-                    
-                // if(lowerJump==0) upperJumpChance=1;
-                // if(upperJump==0) lowerJumpChance=1;
-               
-              
-
-             }
-
-
-
+                fl=!fl;
+            }
+            
         }
-        return String.valueOf(str);
+
+        return String.valueOf(st);
+
+
         
     }
 }
