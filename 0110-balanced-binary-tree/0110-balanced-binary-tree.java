0891de5bfeb890d140b4f5f637d633d42b233742
Achal Vishnoi <161050000+AchalVishnoi@users.noreply.github.com>
2026-02-08 17:31:04 +0530

    Time: 1 ms (30.78%), Space: 45.4 MB (94.54%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -14,49 +14,17 @@
  * }
  */
 class Solution {
-    public boolean isBalanced(TreeNode root) {
+    public boolean isBalanced(TreeNode root){
+
         return check(root)!=-1;
+        
     }
-    
-/*    public boolean isBal(TreeNode root){
-      
-      if(root==null) return true;
-      if(Math.abs(height(root.left)-height(root.right))>1) return false;
-     
-     return isBal(root.left)&&isBal(root.right);
+    public int check(TreeNode root){
+        if(root==null) return 0;
 
+        int lef=check(root.left);
+        int rig=check(root.right);
+        if(lef==-1||rig==-1||Math.abs(lef-rig)>1) return -1;
+        return Math.max(lef,rig)+1;
     }
-
-    public int height(TreeNode root){
-     
-     if(root==null) return 0;
-
-     int lef=height(root.left);
-     int rig=height(root.right);
-
-   
-   return Math.max(lef,rig)+1;
-
-   }
-*/
-
-
-public int check(TreeNode root){
-
-   if(root==null) return 0;
-
-    int lef = check(root.left);
-    if (lef == -1) return -1; 
-
-    int rig = check(root.right);
-    if (rig == -1) return -1;
-
-      if (Math.abs(lef- rig) > 1) return -1;
-   
-   return Math.max(lef,rig)+1;
-
-
-
-}
-
 }
