ec23ce349e4c54ec607408ab40bd839ba9399cd3
Achal Vishnoi <161050000+AchalVishnoi@users.noreply.github.com>
2026-02-11 17:57:59 +0530

    Time: 0 ms (100%), Space: 42.2 MB (49.89%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.java
+++ 0319-bulb-switcher/0319-bulb-switcher.java
@@ -2,15 +2,7 @@ class Solution {
     
     public int bulbSwitch(int n) {
 
-        int ans=0;
-        for(int i=1;i*i<=n;i++){
-           if(i*i<=n) ans++;
-        }
-
-        return ans;
-
-
-        
+        return (int)Math.sqrt(n);
     }
 
 
