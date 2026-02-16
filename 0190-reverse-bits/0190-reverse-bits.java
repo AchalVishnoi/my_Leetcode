2a122b18f4ce4ed96be6acb9a46b3e921ca93140
Achal Vishnoi <161050000+AchalVishnoi@users.noreply.github.com>
2026-02-16 19:18:01 +0530

    Time: 0 ms (100%), Space: 42.8 MB (10.55%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int reverseBits(int n) {
+
+        int ans=0;
+
+        for(int i=31;i>=0;i--){
+
+            if((n&(1<<i))!=0){
+
+                ans|=(1<<(31-i));
+
+            }
+
+        }
+
+        return ans;
+        
+    }
+}
