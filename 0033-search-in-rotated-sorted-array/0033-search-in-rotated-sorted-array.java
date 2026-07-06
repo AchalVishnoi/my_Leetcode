99e3a427718189e52070c9fa688262de3f8e8844
Achal Vishnoi <161050000+AchalVishnoi@users.noreply.github.com>
2026-07-06 20:41:51 +0530

    Time: 0 ms (100%), Space: 43.6 MB (81.85%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -8,24 +8,22 @@ class Solution {
     private int find(int nums[],int tar){
         int n=nums.length;
         int st=0,end=n-1;
-        while(st<end){
+        while(st<=end){
             int mid=st+(end-st)/2;
             int a=nums[st];
             int b=nums[mid];
             int c=nums[end];
             if(b==tar) return mid;
             if(a<=b){
-                if(tar>=a&&tar<=b) end=mid;
+                if(tar>=a&&tar<b) end=mid-1;
                 else st=mid+1;
             }
             else{
-                if(tar>=b&&tar<=c) st=mid;
+                if(tar>b&&tar<=c) st=mid+1;
                 else end=mid-1;  
             }
         }
 
-        if(nums[st]==tar) return st;
-        if(nums[end]==tar) return end;
 
         return -1;
 
