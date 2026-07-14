7e365e865586f319afb84663333dadc08893214e
Achal Vishnoi <161050000+AchalVishnoi@users.noreply.github.com>
2026-07-14 12:43:44 +0530

    Time: 1 ms (29.85%), Space: 47 MB (91.82%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,16 +1,26 @@
 class Solution {
     public int missingNumber(int[] nums) {
-
-    int xor=0;
-    for(int i=1;i<=nums.length;i++){
-        xor^=i;
+        sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=i) return i;
+        }  
+        return nums.length;
     }
-
-    for(int i=0;i<nums.length;i++){
-        xor^=nums[i];
+    public static void sort(int arr[]){
+        int n=arr.length;
+        int i=0;
+        while (i<n){
+            int idx=arr[i];
+            
+            if(idx<n&&arr[idx]!=arr[i]){
+                swap(arr,i,idx);
+            }
+            else i++;
+        }
     }
-
-    return xor;
-        
+    public static void swap(int arr[],int i,int j){
+        int t=arr[i];
+        arr[i]=arr[j];
+        arr[j]=t;
     }
 }
